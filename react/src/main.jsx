ae5ecdd23cbc70eb92775e89a6871a323f9f10d8
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2024-01-27 01:36:17 -0500

    remove cra bloat / organize packages / organize files

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ConfirmProvider } from "material-ui-confirm";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
