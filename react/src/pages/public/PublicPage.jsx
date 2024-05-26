dfd2f061c0c44153e38b180a57352044d5b5f87b
Michael Ortiz <mortiz@redventures.com>
2024-05-25 22:11:42 -0400

    Fix lint

--- react/src/pages/public/PublicPage.jsx
+++ react/src/pages/public/PublicPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '@mui/material-next/Button';
 import { Container, Avatar, CircularProgress, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
